8e3f04733107b89f855f73414cbb5ffc148c7f2d
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-08-28 22:50:53 +0530

    feat: add solutions for basic mathematical operations kata using switch, objects, and if-else

---
+++ 8-kyu/basic-mathematical-operations/basic-mathematical-operations.js
@@ -0,0 +1,41 @@
+// SOLUTION 1 (switch case)
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 'Invalid Operation';
+  }
+}
+
+// SOLUTION 2 (Objects)
+function basicOp(operation, value1, value2) {
+  let cases = {
+    '+': value1 + value2,
+    '-': value1 - value2,
+    '*': value1 * value2,
+    '/': value1 / value2,
+  };
+  return cases[operation];
+}
+
+// SOLUTION 3 (If-Else)
+
+function basicOp(operation, value1, value2) {
+  if (operation === '+') {
+    return value1 + value2;
+  } else if (operation === '-') {
+    return value1 - value2;
+  } else if (operation === '*') {
+    return value1 * value2;
+  } else if (operation === '/') {
+    return value1 / value2;
+  }
+  return 0;
+}
