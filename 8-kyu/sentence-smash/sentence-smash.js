9caf76039b24a1dec75822e7b26b0d7eb1353cb5
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-09-03 23:26:28 +0530

    feat: add solution for (sentence smash) 8 kyu kata

---
+++ 8-kyu/sentence-smash/sentence-smash.js
@@ -0,0 +1,5 @@
+function smash(words) {
+  // The argument `' '` specifies that a space should be placed between each word when joining them.
+  // This effectively concatenates all the elements in the `words` array into a single string with spaces separating them.
+  return words.join(' ');
+}
