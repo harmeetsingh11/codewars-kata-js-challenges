492f8642e225929d3c209d5dd8afbfab869564e3
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-10-06 22:58:21 +0530

    feat: add a solution for (string ends with ?) 7 kyu kata

---
+++ 7-kyu/string-ends-with-?/string-ends-with-?.js
@@ -0,0 +1,6 @@
+function solution(str, ending) {
+  // Use the endsWith() method to check if 'str' ends with the 'ending' string
+  return str.endsWith(ending);
+
+  // endsWith(): This built-in method checks if the string (str) ends with the substring (ending). It returns true if it does, and false otherwise.
+}
