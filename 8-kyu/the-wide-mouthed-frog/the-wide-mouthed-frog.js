7e7aab89efdd7de5f2d5fa2455604daf2f25e4c8
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-10-08 16:15:33 +0530

    feat: add a solution for (the wide mouthed frog) 8 kyu kata

---
+++ 8-kyu/the-wide-mouthed-frog/the-wide-mouthed-frog.js
@@ -0,0 +1,4 @@
+function mouthSize(animal) {
+  // Check if the animal is "alligator" (case-insensitive), return 'small', else 'wide'
+  return animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
+}
