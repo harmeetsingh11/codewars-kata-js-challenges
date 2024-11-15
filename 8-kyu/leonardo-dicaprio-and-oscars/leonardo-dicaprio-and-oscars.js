aa133342be8a16281398fa1cf7d1ea40c67cb9ae
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-11-15 22:12:52 +0530

    feat: add a solution for (Leonardo Dicaprio and Oscars) 8 kyu kata

---
+++ 8-kyu/leonardo-dicaprio-and-oscars/leonardo-dicaprio-and-oscars.js
@@ -0,0 +1,7 @@
+function leo(oscar) {
+  // Check specific conditions based on oscar value and return appropriate response
+  if (oscar === 88) return 'Leo finally won the oscar! Leo is happy';
+  if (oscar === 86) return 'Not even for Wolf of wallstreet?!';
+  if (oscar < 88) return 'When will you give Leo an Oscar?';
+  return 'Leo got one already!';
+}
