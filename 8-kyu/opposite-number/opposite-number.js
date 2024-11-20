7dbe7511c5401c794e5e7848abc5a1448a8113a2
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-11-20 22:26:44 +0530

    feat: add a solution for (Opposite number) 8 kyu kata

---
+++ 8-kyu/opposite-number/opposite-number.js
@@ -0,0 +1,9 @@
+function findOpposite(num) {
+  // Simply negate the input number to get its additive inverse
+  return -num;
+}
+
+/*
+Additive Inverse: The additive inverse of a number is what you add to it to get zero. For any number n, the additive inverse is -n.
+Negation: Using the unary - operator in JavaScript efficiently calculates the additive inverse.
+*/
