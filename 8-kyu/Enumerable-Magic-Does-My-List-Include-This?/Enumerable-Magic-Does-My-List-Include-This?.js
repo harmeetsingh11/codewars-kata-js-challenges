51414b1f0852b54868dd9ff957a1d7e340c1fc36
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-11-22 23:36:01 +0530

    feat: add a solution for (Enumerable Magic - Does My List Include This?) 8 kyu kata

---
+++ 8-kyu/Enumerable-Magic-Does-My-List-Include-This?/Enumerable-Magic-Does-My-List-Include-This?.js
@@ -0,0 +1,6 @@
+function contains(list, item) {
+  // Use the Array.prototype.includes() method to check for item existence.
+  // This method is ideal because it is built into JavaScript, making it both efficient and concise.
+  // It checks if the item exists in the array and returns a boolean (true if found, false otherwise).
+  return list.includes(item);
+}
