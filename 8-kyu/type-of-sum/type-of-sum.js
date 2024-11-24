0ad9aff5ee505f6bd667fad11662779859a14aaa
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-11-24 23:48:37 +0530

    feat: add a solution for (type of sum) 8 kyu kata

---
+++ 8-kyu/type-of-sum/type-of-sum.js
@@ -0,0 +1,11 @@
+function typeOfSum(a, b) {
+  // Return the type of the sum of the two arguments using the `typeof` operator
+  return typeof (a + b);
+}
+
+/*  
+typeof Operator: The typeof operator returns a string indicating the type of the result of the addition:
+"number" if the result is a number.
+"string" if the result is a string.
+"undefined", "object", etc., depending on the result type.
+*/
