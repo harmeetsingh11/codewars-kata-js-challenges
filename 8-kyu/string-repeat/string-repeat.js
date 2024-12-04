7e5f7aa543d176fbdbb2b0a2ce2d8cf40258546f
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-12-04 21:30:44 +0530

    feat: add a solution for (String repeat) 8 kyu kata

---
+++ 8-kyu/string-repeat/string-repeat.js
@@ -0,0 +1,15 @@
+function repeatString(n, s) {
+  // Use the repeat method of the string object to repeat the string 'n' times
+  return s.repeat(n);
+}
+
+/* 
+Explanation:
+
+Input Parameters:
+n: The number of times the string s should be repeated.
+s: The string to be repeated.
+
+Key Method Used:
+The .repeat(n) method is a built-in JavaScript function for strings. It repeats the string n times and concatenates the results.
+*/
