e377d9e1517a61c4d977872fddb96fbd54ea2589
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-12-10 22:49:35 +0530

    feat: add a solution for (Sort arrays - 1) 7 kyu kata

---
+++ 7-kyu/sort-arrays-1/sort-arrays-1.js
@@ -0,0 +1,5 @@
+sortme = function (names) {
+  return names.sort(); // Using the built-in sort() function which sorts strings lexicographically by default
+};
+
+// names.sort(): The sort() method sorts the array in place, using the default string comparison (lexicographical order). This method handles alphabetic sorting automatically.
