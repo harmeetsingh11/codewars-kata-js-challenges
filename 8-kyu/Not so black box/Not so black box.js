480d3f99c6502144e64f9e55f8e15e18f6a8a4b6
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2025-04-12 23:08:07 +0530

    feat: add a solution for (Not so black box) 8 kyu kata

---
+++ 8-kyu/Not so black box/Not so black box.js
@@ -0,0 +1,10 @@
+JSopenSesame();
+
+/*
+💡 Explanation:
+The kata is designed to test lateral thinking — you're not supposed to break down blackBox() directly.
+
+Instead, after calling blackBox(), a new global function appears.
+
+That function is JSopenSesame, and calling it is the key to solving the kata.
+*/
